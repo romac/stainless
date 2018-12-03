5099e1ccd2e650ad3bbf1004b016fc023000fc1e
Romain Ruetschi <romain.ruetschi@gmail.com>
2018-12-03 16:17:22 +0100

    dotc wip

--- frontends/dotty/src/main/scala/stainless/frontends/dotc/CodeExtraction.scala
+++ frontends/dotty/src/main/scala/stainless/frontends/dotc/CodeExtraction.scala
@@ -1315,7 +1315,12 @@ class CodeExtraction(inoxCtx: inox.Context, cache: SymbolsContext)(implicit val 
           case (xt.ArrayType(_), "update",  Seq(index, value)) => xt.ArrayUpdate(extractTree(lhs), extractTree(index), extractTree(value))
           case (xt.ArrayType(_), "clone",   Seq())             => extractTree(lhs)
 
-          case (_, "toString",  Seq()) => xt.ToString(extractTree(lhs))
+          case (
+            xt.StringType() | xt.BooleanType() | xt.IntegerType() | xt.BVType(_, _) |
+            xt.RealType() | xt.SetType(_) | xt.BagType(_) | xt.MapType(_, _) | xt.TypeParameter(_, _),
+            "toString",
+            Seq()
+          ) => xt.ToString(extractTree(lhs), tpe)
 
           case (xt.MapType(_, _), "get", Seq(rhs)) =>
             xt.MapApply(extractTree(lhs), extractTree(rhs))
