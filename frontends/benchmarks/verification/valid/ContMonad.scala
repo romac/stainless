8331339730e6d09fd9ba55c23292d19fc6119817
Romain Ruetschi <romain.ruetschi@gmail.com>
2019-01-16 11:05:11 +0100

    wip

--- frontends/benchmarks/verification/valid/ContMonad.scala
+++ frontends/benchmarks/verification/valid/ContMonad.scala
@@ -3,9 +3,24 @@ import stainless.annotation._
 
 object ContMonad {
 
+  sealed abstract class ===[A, B] {
+    @inline def cast(a: A): B
+    @inline def flip: B === A
+  }
+
+  case class Refl[A]() extends (A === A) {
+    @inline override def cast(a: A): A = a
+    @inline override def flip: A === A = this
+  }
+
+  implicit def refl[A]: A === A = Refl[A]()
+
   case class Cont[R, A](runCont: (A => R) => R) {
     @inline
-    def map[B](f: A => B): Cont[R, B] = Cont { (k: B => R) =>
+    def get(implicit ev: A === R): R = runCont(a => ev.cast(a))
+
+    @inline
+    def map[B](f: A => B): Cont[R, B] = Cont {(k: B => R) =>
       runCont((a: A) => k(f(a)))
     }
 
@@ -29,18 +44,23 @@ object ContMonad {
   def cont[R, A](f: (A => R) => R): Cont[R, A] = Cont(f)
 
   // @inline
-  def callCC[R, A, B](f: (A => Cont[R, B]) => Cont[R, A]): Cont[R, A] = cont { k =>
-    f(a => cont(_ => k(a))).runCont(k)
+  def callCC[R, A, B](f: (A => Cont[R, B]) => Cont[R, A]): Cont[R, A] = cont {
+    k =>
+      f(a => cont(_ => k(a))).runCont(k)
   }
 
   object FunctorLaws {
-    @library
     def functorIdentity[R, A](c: Cont[R, A], k: A => R): Boolean = {
       c.map((x: A) => x).runCont(k) == c.runCont(k)
     }.holds
 
     @library
-    def functorAssociativity[R, A, B, C](c: Cont[R, A], f: A => B, g: B => C, k: C => R): Boolean = {
+    def functorAssociativity[R, A, B, C](
+        c: Cont[R, A],
+        f: A => B,
+        g: B => C,
+        k: C => R
+    ): Boolean = {
       c.map(f).map(g).runCont(k) == c.map(a => g(f(a))).runCont(k)
     }.holds
   }
@@ -54,21 +74,42 @@ object ContMonad {
     a / b
   }
 
+  def test(a: Int, b: Int): Int = {
+    val res = try {
+      Some(div(a, b))
+    } catch {
+      case DivideByZero() => None
+    }
+
+    res.getOrElse(42)
+  }
+
   */
 
   @library
   def div[R](a: Int, b: Int, k: DivideByZero => Cont[R, Int]): Cont[R, Int] =
     callCC[R, Int, DivideByZero] { (ok: Int => Cont[R, DivideByZero]) =>
-      val c: Cont[R, DivideByZero] = callCC[R, DivideByZero, Unit] { (notOk: DivideByZero => Cont[R, Unit]) =>
-        val d: Cont[R, Unit] = Cont.whenCond(b == 0)(notOk(DivideByZero()))
-        d flatMap { _ =>
-          ok(a / b)
-        }
+      val c: Cont[R, DivideByZero] = callCC[R, DivideByZero, Unit] {
+        (notOk: DivideByZero => Cont[R, Unit]) =>
+          val d: Cont[R, Unit] = Cont.whenCond(b == 0)(notOk(DivideByZero()))
+          d flatMap { _ =>
+            ok(a / b)
+          }
       }
       c flatMap { err =>
         k(err)
       }
     }
+
+  def isOk(a: Int, b: Int): Cont[Int, Int] = {
+    require(b != 0)
+    div(a, b, err => Cont.pure[Int, Int](0))
+  } ensuring { _.runCont(a => a) == (a / b) }
+
+  def isNotOk(a: Int, b: Int): Cont[Int, Int] = {
+    require(b == 0)
+    div(a, b, err => Cont.pure[Int, Int](0))
+  } ensuring { _.runCont(a => a) == 0 }
 
   def testOk = {
     assert(div(10, 2, err => Cont.pure[Int, Int](0)).runCont(a => a) == 5)
